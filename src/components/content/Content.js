9f8e353ef4ff06563890b523655a17860e4d9c3a
Lucas Illiano <lucas.illiano@hotmail.com>
2021-04-10 11:07:39 +0200

    insert of app structure from layout

---
+++ src/components/content/Content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Content.scss";
+
+function Content() {
+  return (
+    <div className="content">
+      Content
+    </div>
+  )
+}
+
+export default Content
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.scss";
+function Footer() {
+  return (
+    <div className="footer">
+      <div class="footer-content">
+        <p>Macron Fact App 2021 by @elviajero</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
